680486cbb1fb3af836d5fcd8928717a7d735e422
IR-System <IR-System@host.docker.internal>
2020-11-02 17:03:18 +0100

    add and delete by id added

--- todo-api/src/main/java/ly/taha/springboot/todos/TodoController.java
+++ todo-api/src/main/java/ly/taha/springboot/todos/TodoController.java
@@ -3,8 +3,11 @@ package ly.taha.springboot.todos;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,4 +27,17 @@ public class TodoController {
 	public Todo getTodoById(@PathVariable int id) {
 		return todoService.getById(id);
 	}
-}
+	
+	@PostMapping(value = {"","/"})
+	public Todo createNewTodo(@RequestBody Todo todo) {
+		if (todoService.save(todo)) {
+		return todo;
+		}
+		return null;
+	}
+	
+	@DeleteMapping("/{id}")
+	public void deleteTodo(@PathVariable int id) {
+		todoService.delete(id);
+	}
+}	
